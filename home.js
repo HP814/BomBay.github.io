8ed423d1645d884f3e24386b603cb6441183d30d
HP814 <98508298+HP814@users.noreply.github.com>
2023-02-04 13:22:00 +0530

    uploaded files

---
+++ home.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+ distance: '60px',
+ duration: 2500,
+ delay: 400
+});
+
+      
+ScrollReveal().reveal('.main-title', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.sec-01 .image', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.text-box', { delay: 700, origin: 'right' });
+ScrollReveal().reveal('.sec-02 .image, .sec-03 .image ,.sec-04 .image, .sec-05 .image, .sec-06 .image, .sec-07 .image', { delay: 500, origin: 'top' });
+      
